aa6c3a54fa6a9b6a04242fa4497c29c82a3e999b
Malik Azad <113776072+malik-azad@users.noreply.github.com>
2024-05-01 23:30:31 +0530

    Add files via upload

---
+++ Greatestnumber.py
@@ -0,0 +1,12 @@
+def findGreaterNumber(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num2 > num1:
+        return num2
+    else:
+        return "Both Number are equal" 
+
+    num1 = int(input("Enter the first number")) 
+    num2 = int(input("Enter the second number"))
+    greatestNumber = findGreaterNumber(num1, num2)
+    print("The greater number is: ", greatestNumber)       
